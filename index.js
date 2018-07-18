1e41b3d8a9ffefa1e29905a5ad041b83e65eec9e
asdcpi70914 <asdcpi70914@gmail.com>
2018-07-18 14:48:21 +0800

    test

--- index.js
+++ index.js
@@ -22375,7 +22375,7 @@ bot.on("message",function(event){
     		Sname: a_instr,
     		Grade: b_instr
 			};
-		connection.query('INSERT INTO `'+ STUDENT +'` SET ?', data, function(error){
+		connection.query('INSERT INTO `'+ table+'` SET ?', data, function(error){
     	if(error){
         	console.log('寫入資料失敗！');
         	throw error;
