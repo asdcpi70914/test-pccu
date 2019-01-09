ff16a498d153dc886006e1134233059133028fb5
asdcpi70914 <asdcpi70914@gmail.com>
2019-01-10 07:12:58 +0800

    test

--- app.js
+++ app.js
@@ -158,7 +158,7 @@ app.get("/form1", function(request, response,next) {
   response.render("form1")
 }); 
 app.get("/index", function(request, response,next) {
-  console.log(request.headers.cookie)
+  console.log(request)
   response.render("index")
 }); 
 app.get("/index1", function(request, response) {
