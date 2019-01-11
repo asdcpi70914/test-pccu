e759569662abd3612767ca567adff233c4e571e0
asdcpi70914 <asdcpi70914@gmail.com>
2019-01-11 21:24:05 +0800

    make it better

--- app.js
+++ app.js
@@ -302,19 +302,29 @@ app.post("/index3", function(request, response) {
          }
          console.log("select Sname and Student_ID");
          var countname = 0;
+         for(var x in results){
+ 
+           studentname.push(results[x].Student_ID);
+           console.log("sn:"+studentname[countname]);
+           countname = countname+1;
+         }
+      });
+
     setTimeout(function(){ 
-      for(var i = 0 ; i < results.length ; i++){
-        connection.query('INSERT INTO Grade (Transcript_ID,Student_ID,Grade,Course_ID,GDATE) VALUES ((SELECT Transcript_ID FROM Transcript WHERE TranscriptName = ?),?,?,(Select Course_ID FROM COURSE WHERE CourseName = ?),?)',[time1,results[i].Student_ID,"",classname,today], function(err, results) {
+      console.log("studentname.length:"+studentname.length);
+      console.log("studentname:"+studentname[0]);
+      for(var i = 0 ; i < studentname.length ; i++){
+        connection.query('INSERT INTO Grade (Transcript_ID,Student_ID,Grade,Course_ID,GDATE) VALUES ((SELECT Transcript_ID FROM Transcript WHERE TranscriptName = ?),?,?,(Select Course_ID FROM COURSE WHERE CourseName = ?),?)',[time1,studentname[i],"",classname,today], function(err, results) {
           if (err) {
             throw err;
           }
           console.log("資料已輸入");
         });
       }
+    connection.end();
+  response.render("index3");
      }, 4000);
-    response.render("index3");
-  });
-          connection.end();  
+
 });
 var data = {};
 var status = ""
